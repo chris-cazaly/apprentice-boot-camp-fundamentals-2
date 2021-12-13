2215ca2acd5302b934c62a1c241725bd0a7ea006
Dave Aspinall <dave.aspinall@autotrader.co.uk>
2021-12-13 15:45:02 +0000

    feat: calculate score for game containing a strike

--- exercises/javascript/main/bowling/bowling.js
+++ exercises/javascript/main/bowling/bowling.js
@@ -1,3 +1,45 @@
 export const bowl = (rolls) => {
-  return 0;
+  let total = 0;
+  const frames = [];
+
+  for (let i = 0; i < rolls.length; i += 2) {
+    if (rolls[i] === 10) {
+      frames.push([rolls[i]]);
+      i--;
+    } else {
+      frames.push([rolls[i], rolls[i + 1]]);
+    }
+  }
+
+  for (let i = 0; i < frames.length; i++) {
+    const currentFrame = frames[i];
+
+    let scoreForFrame;
+
+    const isStrike = currentFrame.length === 1;
+    // const scoreFromCurrentFrame = isStrike ? currentFrame[0] : currentFrame[0] + currentFrame[1]
+    // const scoreFromNextFrame = isStrike ? nextFrame[0] + nextFrame[1] : nextFrame[0]
+    // condition ? true : false
+
+    if (isStrike) {
+      scoreForFrame = currentFrame[0];
+
+      const nextFrame = frames[i + 1];
+      scoreForFrame += nextFrame[0] + nextFrame[1];
+    } else {
+      scoreForFrame = currentFrame[0] + currentFrame[1];
+      const isSpare = scoreForFrame === 10;
+      const nextFrame = frames[i + 1];
+
+      if (isSpare) {
+        scoreForFrame += nextFrame[0];
+      }
+    }
+
+    total += scoreForFrame;
+  }
+
+  return total;
+
+  //   return rolls.reduce((score, roll) => score + roll, 0);
 };
--- exercises/javascript/test/bowling/bowling.test.js
+++ exercises/javascript/test/bowling/bowling.test.js
@@ -3,6 +3,38 @@ import { bowl } from "../../main/bowling/bowling";
 describe("Bowling tests", () => {
   it("should calculate score of zero when zero pins are hit", () => {
     const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    expect(bowl(rolls)).toBe(0);
+    const results = bowl(rolls);
+    expect(results).toBe(0);
+  });
+
+  it("should calculate score for a regular game of bowling (no strikes or spares)", () => {
+    const rollsOne = [
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    ];
+    const resultsOne = bowl(rollsOne);
+    expect(resultsOne).toBe(20);
+
+    const rollsTwo = [
+      0, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ];
+    const resultsTwo = bowl(rollsTwo);
+    expect(resultsTwo).toBe(11);
+  });
+
+  it("should calculate score for a regular game containing a spare", () => {
+    const rolls = [5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const result = bowl(rolls);
+    expect(result).toBe(12);
+
+    const rolls2 = [5, 5, 1, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const resultsTwo = bowl(rolls2);
+
+    expect(resultsTwo).toBe(23);
+  });
+
+  it("should calculate score for a regular game containing a strike", () => {
+    const rolls = [10, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const result = bowl(rolls);
+    expect(result).toBe(14);
   });
 });
