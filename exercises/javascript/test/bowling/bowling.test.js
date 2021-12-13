f90c8b56443e70438d767df0591c3659bf324665
Dave Aspinall <dave.aspinall@autotrader.co.uk>
2021-12-13 11:52:58 +0000

    feat: calculate bowling score when zero pins are hit

--- exercises/javascript/test/bowling/bowling.test.js
+++ exercises/javascript/test/bowling/bowling.test.js
@@ -1,5 +1,8 @@
-describe('Bowling tests', () => {
-  test('Rename me', () => {
+import { bowl } from "../../main/bowling/bowling";
 
+describe("Bowling tests", () => {
+  it("should calculate score of zero when zero pins are hit", () => {
+    const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    expect(bowl(rolls)).toBe(0);
   });
 });
